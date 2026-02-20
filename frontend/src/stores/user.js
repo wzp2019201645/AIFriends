b5d4eea4e9427b29ed3980969fa3c90139e58ef0
Zhongpeng Wang <1152781631@qq.com>
2026-02-20 16:26:13 +0800

    实现下拉菜单

---
+++ frontend/src/stores/user.js
@@ -0,0 +1,46 @@
+import {defineStore} from "pinia";
+import {ref} from "vue";
+
+export const useUserStore = defineStore('user', () => {
+    const id = ref(0)
+    const username = ref('')
+    const photo = ref('')
+    const profile = ref('')
+    const accessToken = ref('')
+
+    function isLogin() {
+        return !!accessToken.value  // 必须带value!
+    }
+
+    function setAccessToken(token) {
+        accessToken.value = token
+    }
+
+    function setUserInfo(data) {
+        id.value = data.user_id
+        username.value = data.username
+        photo.value = data.photo
+        profile.value = data.profile
+    }
+
+    function logout() {
+        id.value = 0
+        username.value = ''
+        photo.value = ''
+        profile.value = ''
+        accessToken.value = ''
+    }
+
+    return {
+        id,
+        username,
+        photo,
+        profile,
+        accessToken,  // 千万不要忘了!
+        isLogin,
+        setAccessToken,
+        setUserInfo,
+        logout,
+    }
+})
+
